f4c37fda8143dd139d8d01da9db98171a6951e32
Pete Rai <pete@rai.org.uk>
2021-10-20 17:34:35 +0100

    Now with spoiler warning support

--- reel-it.sh
+++ reel-it.sh
@@ -38,6 +38,12 @@ DESC_2=$(bc -l <<< "$DESC_TOP + 1 * $DESC_LINE_GAP")
 DESC_3=$(bc -l <<< "$DESC_TOP + 2 * $DESC_LINE_GAP")
 AUDIO_FADE_SECS=1
 BACKGROUND="./images/background.jpg"
+SPOILER="./images/spoiler.png"
+SPOILER_WIDTH=1000
+SPOILER_HEIGHT=333
+SPOILER_IN=0.75
+SPOILER_OUT=5.5
+SPOILER_FADE=1
 
 # --- input file parameters
 
@@ -49,6 +55,7 @@ txt3=$(cat "$1" | jq -r '.txt3')
 file=$(cat "$1" | jq -r '.file')
 from=$(cat "$1" | jq -r '.from')
 time=$(cat "$1" | jq -r '.time')
+warn=$(cat "$1" | jq -r '.warn')
 
 # --- calculated parameters
 
@@ -56,6 +63,7 @@ filename=$(basename "$1" .json)
 input="./masters/$file.mp4"
 output="./output/$filename.mp4"
 extract="./extract.mp4"
+stamped="./stamped.mp4"
 mounted="./mounted.mp4"
 
 from_secs=$(echo $from | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
@@ -64,8 +72,16 @@ fade_point=$(expr $from_secs + $time_secs - $AUDIO_FADE_SECS)
 
 size_x=$(ffprobe -v error -select_streams v:0 -show_entries stream=width  -of csv=s=x:p=0 $input)
 size_y=$(ffprobe -v error -select_streams v:0 -show_entries stream=height -of csv=s=x:p=0 $input)
-pos_y=$(bc -l <<< "$MOVIE_MID_HEIGHT - (($MOVIE_WIDTH / $size_x * $size_y) / 2)")
+MOVIE_HEIGHT=$(bc -l <<< "$MOVIE_WIDTH / $size_x * $size_y")
+
+pos_y=$(bc -l <<< "$MOVIE_MID_HEIGHT - ($MOVIE_HEIGHT / 2)")
 pos_y=$(echo $pos_y | awk '{ print int($1 + 0.5) }')
+
+spoil_x=$(bc -l <<< "($MOVIE_WIDTH - $SPOILER_WIDTH) / 2")
+spoil_x=$(echo $spoil_x | awk '{ print int($1 + 0.5) }')
+
+spoil_y=$(bc -l <<< "($MOVIE_HEIGHT - $SPOILER_HEIGHT) / 2")
+spoil_y=$(echo $spoil_y | awk '{ print int($1 + 0.5) }')
 
 # --- start processing video
 
@@ -78,17 +94,31 @@ rm $output 2> /dev/null
 echo "  extracting..."
 ffmpeg     -i $input \
           -ss $from -t $time \
-       -lavfi "pad= w=$BORDER_SIZE_FULL+iw : h=$BORDER_SIZE_FULL+ih : x=$BORDER_SIZE : y=$BORDER_SIZE : color=$BORDER_COLOR; \
-               afade= t=out : st=$fade_point : d=$AUDIO_FADE_SECS" \
+       -lavfi "[0]scale= $MOVIE_WIDTH:-1[i]; \
+               [i]pad= w=$BORDER_SIZE_FULL+iw : h=$BORDER_SIZE_FULL+ih : x=$BORDER_SIZE : y=$BORDER_SIZE : color=$BORDER_COLOR; \
+                afade= t=out : st=$fade_point : d=$AUDIO_FADE_SECS" \
            -v error \
            -y $extract
 
+if [ "$warn" = true ]; then
+   echo "  stamping..."
+   ffmpeg     -i $extract \
+      -loop 1 -i $SPOILER \
+     -lavfi "[1]format=yuva420p,fade=in:st=$SPOILER_IN:d=$SPOILER_FADE:alpha=1,fade=out:st=$SPOILER_OUT:d=$SPOILER_FADE:alpha=1[i]; \
+             [0][i]overlay=$spoil_x:$spoil_y:shortest=1" \
+       -c:a copy \
+         -v error \
+         -y $stamped
+else
+    cp $extract $stamped
+fi
+
 echo "  mounting..."
-   ffmpeg  -loop 1 -i $BACKGROUND \
-           -i $extract \
-       -lavfi "[1]scale=$MOVIE_WIDTH:-1[inner]; \
-               [0][inner]overlay=$MOVIE_LEFT:$pos_y:shortest=1[out]" \
-         -map "[out]" -map 1:a -c:a copy \
+ffmpeg  -loop 1 -i $BACKGROUND \
+           -i $stamped \
+       -lavfi "[0][1]overlay=$MOVIE_LEFT:$pos_y:shortest=1[out]" \
+         -map "[out]" -map 1:a \
+         -c:a copy \
            -v error \
            -y $mounted
 
@@ -103,7 +133,9 @@ ffmpeg    -i $mounted \
           -y $output
 
 rm $extract
+rm $stamped
 rm $mounted
+
 open $output
 
 echo
